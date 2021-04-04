6b6233e2fbff689b8318f25ef9e7be8ddd74707b
noah <noahfrmc@gmail.com>
2021-04-04 00:18:01 -0700

    riced dwm to more match my mushroom background

--- config.h
+++ config.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "fontawesome:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=11", "fontawesome:size=11" };
+static const char dmenufont[]       = "monospace:size=11";
 //background color
 static const char col_gray1[]       = "#222222";
 //inactive window border color 
@@ -16,8 +16,15 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 //current tag and current window font color
 static const char col_gray4[]       = "#eeeeee";
+
 //Top bar second color (blue) and active window border color
-static const char col_cyan[]        = "#005577";
+
+//darker blue original color
+//static const char col_cyan[]        = "#005577";
+
+//green 
+static const char col_cyan[]        = "#53ad45";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
