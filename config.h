0aaff8a2e478152e140304f8e6e263159f89edcd
noah <noahfrmc@gmail.com>
2021-04-04 16:22:41 -0700

    updated the size

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=11", "fontawesome:size=11" };
-static const char dmenufont[]       = "monospace:size=11";
+static const char *fonts[]          = { "monospace:size=12", "fontawesome:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 //background color
 static const char col_gray1[]       = "#111111";
 //inactive window border color 
