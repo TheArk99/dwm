97fc095071a0e1c503bff0797a8b45eb2ce34168
noah <noahfrmc@gmail.com>
2021-04-04 19:07:49 -0700

    adds a next and preivous tag function

--- config.h
+++ config.h
@@ -75,6 +75,7 @@ static const char *browsercmd[]  = { "brave", NULL };
 static const char *filebrowsercmd[]  = { "pcmanfm", NULL };
 
 
+#include "shiftview.c"
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -108,6 +109,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+  {MODKEY,                        XK_n,      shiftview,      {.i = +1 } },
+  {MODKEY,                        XK_b,      shiftview,      {.i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
