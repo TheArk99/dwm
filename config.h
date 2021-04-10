6523d18d3d506974cb437d5f3c45c1a94284cd63
noah <noahfrmc@gmail.com>
2021-04-09 23:25:42 -0700

    update

--- config.h
+++ config.h
@@ -32,8 +32,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "8", "9" };
-static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", };
+static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", };
  
 
 static const Rule rules[] = {
